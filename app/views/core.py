e40869a9acb55a273a0bef21da3411e6037cd5ad
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2022-01-07 11:04:24 -0300

    opt

--- app/views/core.py
+++ app/views/core.py
@@ -9,6 +9,7 @@ class EngineModel(object):
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--disable-blink-features")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument('--disable-gpu')
         options.add_argument('--incognito')
